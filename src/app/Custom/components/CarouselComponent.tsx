a0c084597f9b5550e0d3b0c767a75f41029901ae
Adel Chafic Gannem <adelchaficgc@gmail.com>
2024-08-24 00:16:55 -0400

    Changes

--- src/app/Custom/components/CarouselComponent.tsx
+++ src/app/Custom/components/CarouselComponent.tsx
@@ -1,43 +1,62 @@
-'use client';
-import * as React from "react"
-import Autoplay from "embla-carousel-autoplay"
- 
-import { Card, CardContent } from "@/components/ui/card"
+"use client";
+import * as React from "react";
+import Autoplay from "embla-carousel-autoplay";
+
+import { Card, CardContent } from "@/components/ui/card";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
- 
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import TShirts from "@/app/assets/Carousel/T-Shirts.png";
+import Xbox from "@/app/assets/Carousel/xbox.png";
+import PC from "@/app/assets/Carousel/PC.png";
+import Celular from "@/app/assets/Carousel/Celular.png";
+import Cafetera from "@/app/assets/Carousel/Cafetera.png"
+
+import Image from "next/image";
+
 export function CarouselComponent() {
-  const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
-  )
- 
-  return (
-    <Carousel
-      plugins={[plugin.current]}
-      className="w-full max-w-xs"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-6xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
+	const Imagenes = [
+		{ id: "1", url: TShirts, name: "Camisas", price: "10$" },
+		{ id: "2", url: Xbox, name: "xbox One", price: "190$" },
+		{ id: "3", url: PC, name: "PC Intel® Core™ i7-1165G7", price: "500$" },
+		{ id: "4", url: Celular, name: "Iphone 15", price: "450$" },
+		{ id: "5", url: Cafetera, name: "Cafetera Oster", price: "120$" },
+	];
+
+	const plugin = React.useRef(Autoplay({ delay: 2500, stopOnInteraction: true }));
+
+	return (
+		<Carousel
+			plugins={[plugin.current]}
+			className="w-32 xl:w-64"
+			onMouseEnter={plugin.current.stop}
+			onMouseLeave={plugin.current.reset}
+		>
+			<CarouselContent>
+				{Imagenes.map((_, index) => (
+					<CarouselItem key={index}>
+						<div className="p-1">
+							<Card>
+								<h2 className="p-10 text-lg 2xl:text-6xl font-bold font-bebas">{Imagenes[index]["name"]}</h2>
+								<CardContent className="flex aspect-square items-center justify-center p-6">
+									<div>
+										<Image src={Imagenes[index]["url"]} alt="" height={500} />
+									</div>
+								</CardContent>
+
+								<h2 className="p-6 text-2xl font-bold font-bebas">{Imagenes[index]["price"]}</h2>
+							</Card>
+						</div>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+      <CarouselNext/>
+      <CarouselPrevious/>
+		</Carousel>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,49 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardTitle } from "@/components/ui/card";
+import { Card, CardDescription, CardFooter, CardTitle } from "@/components/ui/card";
 
+import Image from "next/image";
+import { CarouselComponent } from "./Custom/components/CarouselComponent";
 export default function Home() {
 	return (
-		<main className="min-h-screen bg-neutral-200 p-24 dark:bg-neutral-700 ">
-			<div className="flex flex-col gap-10">
-				<div>
-					<h1 className="text-6xl font-bold font-bebas">Templade</h1>
-					<h3 className="text-3xl font-bold font-patrick">Fast, Easy & Secure</h3>
-					<h3 className="text-xl font-bold font-patrick">
-						the most easy way to buy online, secure and fast.
-					</h3>
-				</div>
-				<div>
+		<main className="flex sm:flex-row flex-col">
+			<div className="flex bg-slate-100 w-100 p-12 lg:p-24 2xl:p-32 dark:bg-neutral-700 ">
+				<div className="flex flex-col gap-10">
 					<div>
-						<Card className="flex flex-col justify- gap-5 p-4 w-[210px] h-auto dark:bg-neutral-700 self-center bg-neutral-100">
-							<CardTitle className="text-2sm text-bebas font-bold">
-								Don't have an account?
-							</CardTitle>
-							<hr />
-							<CardDescription className="text-sm font-patrick font-bold ">
-								Don't waste your time and join us, we will give you exactly what you need.
-							</CardDescription>
-							<CardFooter>
-								<Button className="font-ubuntu h-5 justify-self-center text-sm  ">
+						<h1 className="text-6xl font-bold font-bebas">Templade</h1>
+						<h3 className="text-4xl font-bold font-patrick">Fast, Easy & Secure</h3>
+						<h3 className="text-2xl font-bold font-patrick">
+							the most easy way to buy online, secure and fast.
+						</h3>
+					</div>
+					<div>
+						<div>
+							<Card className="flex flex-col justify- gap-5 p-4 w-[250px] h-auto dark:bg-neutral-700 self-center bg-neutral-100">
+								<CardTitle className="text-lg text-bebas font-bold">
+									Don't have an account?
+								</CardTitle>
+								<hr />
+								<CardDescription className="text-2sm font-patrick font-bold ">
+									Don't waste your time and join us, we will give you exactly what you need.
+								</CardDescription>
+								<Button className="font-ubuntu h-8 justify-self-center text-sm  ">
 									Sign Up Now!
 								</Button>
-							</CardFooter>
-						</Card>
+							</Card>
+							<p className="text-gray-400 dark:text-gray-300 text-sm font-ubuntu py-4 px-2">
+								Already have an account?{" "}
+								<a className="hover:text-gray-800 dark:hover:text-gray-100" href="/">
+									Sign In!
+								</a>
+							</p>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div></div>
+			<div className="px-4 sm:px-16  p-6 lg:p-12    bg-[url('/bg.jpg')] flex justify-center bg-no-repeat bg-cover bg-center bg-fixed bg-blend-multiply bg-slate-500 dark:bg-slate-700 w-full min-h-screen min-h-screen-100%">
+				<div>
+					<CarouselComponent />
+				</div>
+			</div>
 		</main>
 	);
 }
